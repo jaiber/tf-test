046ae5c9d4f3048af97143c6959c7fdf3ff458b9
Jaiber John <jaiber@remote-x.ai>
2023-12-15 20:31:43 +0530

    Further cleanups, removing duplicate code

--- simple-transformer.py
+++ simple-transformer.py
@@ -2,7 +2,9 @@
 
 import sys
 import argparse
+import logging
 import numpy as np
+from gato import Gato, Transformer
 import tensorflow as tf
 from config import GatoConfig
 from loader import DataLoader
@@ -12,115 +14,21 @@ from tensorflow.keras import layers, models, activations
 from tensorflow.keras.optimizers import Adam, AdamW
 
 
-# From Gato
-class TransformerBlock(Model):
-    def __init__(
-        self,
-        config: Union[GatoConfig, Dict[str, Any]],
-        trainable: bool = True,
-        name: str = None,
-        **kwargs,
-    ):
-        super(TransformerBlock, self).__init__(trainable=trainable, name=name, **kwargs)
-        if isinstance(config, dict):
-            config = GatoConfig(**config)
-        self.config = config
-
-        self.attention = self.feed_forward = self.dropout = None
-        self.layer_norm1 = self.layer_norm2 = None
-
-    def build(self, input_shape):
-        input_shape = tf.TensorShape(input_shape)
-        hidden_size = input_shape[-1]
-
-        self.attention = layers.MultiHeadAttention(
-            num_heads=self.config.num_attention_heads,
-            key_dim=self.config.key_value_size,
-            value_dim=self.config.key_value_size,
-            dropout=self.config.dropout_rate,
-            name="attention",
-        )
-        self.dropout = layers.Dropout(self.config.dropout_rate, name="attention_dropout")
-        self.feed_forward = models.Sequential(
-            layers=[
-                layers.Dense(
-                    units=self.config.feedforward_hidden_size,
-                    activation="linear",
-                    name="dense_intermediate",
-                ),
-                # Appendix C.1. Transformer Hyperparameters
-                # Activation Function: GEGLU
-                layers.Lambda(lambda x: activations.gelu(x, approximate=False), name="gelu"),
-                layers.Dropout(self.config.dropout_rate, name="dropout_intermediate"),
-                layers.Dense(units=hidden_size, activation="linear", name="dense"),
-                layers.Dropout(self.config.dropout_rate, name="dropout"),
-            ],
-            name="feed_forward",
-        )
-        self.layer_norm1 = layers.LayerNormalization(epsilon=1e-6, name="layer_norm1")
-        self.layer_norm2 = layers.LayerNormalization(epsilon=1e-6, name="layer_norm2")
-
-    def call(self, inputs):
-        # Appendix C.1. Transformer Hyperparameters
-        # Layer Normalization: Pre-Norm
-        residual = inputs
-        x = self.layer_norm1(inputs)
-        x = self.attention(x, x, x)
-        x = self.dropout(x)
-        x = x + residual
-
-        residual = x
-        x = self.layer_norm2(inputs)
-        x = self.feed_forward(x)
-        x = x + residual
-
-        return x
-
-    def get_config(self):
-        config = super(TransformerBlock, self).get_config()
-        config.update(
-            {
-                "config": self.config.to_dict(),
-            }
-        )
-        return config
-
-
-class Transformer(models.Model):
-    def __init__(
-        self,
-        config: Union[GatoConfig, Dict[str, Any]],
-        trainable: bool = True,
-        name: str = None,
-        **kwargs,
-    ):
-        super(Transformer, self).__init__(trainable=trainable, name=name, **kwargs)
-        if isinstance(config, dict):
-            config = GatoConfig(**config)
-        self.config = config
-        self.encoders = [
-            TransformerBlock(
-                config=self.config, trainable=trainable, name="EncoderBlock{}".format(idx)
-            )
-            for idx in range(self.config.num_transformer_blocks)
-        ]
-
-    def call(self, inputs, training=None, mask=None):
-        x = inputs
-        for encoder in self.encoders:
-            x = encoder(x)
-        return x
-
-    def get_config(self):
-        return super(Transformer, self).get_config()
-
-
 if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO)
+
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--batch_size", type=int, default=32)
     args = parser.parse_args()
+
+    print("Loading data") # Hardcoded for now
+    file = "experiments/Cuboid100Episodes/MasterJsonForEpisodes2023-12-08_22-24-37-645.json"
+    data = DataLoader(file)
+    (all_ids, all_encoding, all_row_pos, all_col_pos, all_obs) = data.load()
+
 
     config = GatoConfig.small()
 
