New baseline - fixes and checks, no convergence and softmax issues

gato/models/__init__.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 import tensorflow as tf
 
 from gato.models.transformer import TransformerBlock
@@ -40,7 +41,8 @@ class Gato(models.Model):
         )
 
         self.flatten = layers.Flatten()
-        self.dense = layers.Dense(3, activation="softmax", name="Output")
+        self.dense = layers.Dense(3, activation="linear", name="Output")
+        self.softmax = layers.Softmax()
 
     def call(self, inputs, training=None, mask=None):
         # input_ids with (B, L, 768)
@@ -79,7 +81,8 @@ class Gato(models.Model):
         encoding = tf.one_hot(encoding, depth=3, dtype=tf.float32)
 
         ones = tf.ones(
-            (input_ids.shape[0], input_ids.shape[0], self.config.layer_width), dtype=tf.float32
+            (input_ids.shape[0], input_ids.shape[0], self.config.layer_width),
+            dtype=tf.float32,
         )
         image_embed = self.image_embedding((input_ids, (row_pos, col_pos)), training=training)
         image_embed *= encoding[..., 0].transpose().matmul(ones)  # image patch masking
@@ -101,13 +104,9 @@ class Gato(models.Model):
         output = self.flatten(hidden_states)
         # Add dense softmax layer
         output = self.dense(output)
-        # argmax
-        # output = tf.argmax(output, axis=-1)
-        # print("output shape: ", output.shape)
-        # Match the size of y_train
+        output = self.softmax(output)
+        # tf.print(">> output: ", output)
         output = tf.expand_dims(output, axis=1)
-        # print("output shape: ", output.shape)
-        # tf.print(output)
         return output
 
     def train_transformer(
@@ -143,7 +142,9 @@ class Transformer(models.Model):
         self.config = config
         self.encoders = [
             TransformerBlock(
-                config=self.config, trainable=trainable, name="EncoderBlock{}".format(idx)
+                config=self.config,
+                trainable=trainable,
+                name="EncoderBlock{}".format(idx),
             )
             for idx in range(self.config.num_transformer_blocks)
         ]

loader.py
@@ -165,9 +165,9 @@ class DataLoader:
     def process_episode(self, episode_config_file, prefix):
         """Process episode config file and create input_ids tensor"""
 
-        input_ids = None
-        input_array = []
-        # discrete_array = tf.Variable(tf.constant([], shape=(0, 1), dtype=tf.int32))
+        input_ids = (
+            None  # tf.Variable(tf.constant([], shape=(0, 1, self.input_dim), dtype=tf.float32))
+        )
         discrete_array = []
 
         logging.debug("Loading episode_config: %s", episode_config_file)
@@ -194,16 +194,30 @@ class DataLoader:
                 else:
                     discrete_array.append(key["action"])
 
-                input_array.append(image)
-                input_array.append(continuous_value)
-                input_array.append(discrete_value)
+                # convert to tensor
+                image = tf.constant(image, dtype=tf.float32)
+                continuous_value = tf.constant(continuous_value, dtype=tf.float32)
+                discrete_value = tf.constant(discrete_value, dtype=tf.float32)
+                # input_array.append(image)
+                # input_array.append(continuous_value)
+                # input_array.append(discrete_value)
+                if input_ids is None:
+                    input_ids = tf.concat([image, continuous_value, discrete_value], axis=1)
+                else:
+                    input_ids = tf.concat(
+                        [input_ids, image, continuous_value, discrete_value], axis=1
+                    )
 
-            logging.debug("input_array size: %s", len(input_array))
-            input_ids = tf.concat(
-                input_array,  # repeat num_observations times
-                axis=1,
-            )
+            logging.info("input_array shape: %s", input_ids.shape)
+            # for i in range(input_ids.shape[1]):
+            #    tf.print("input_ids: ", input_ids[0][i], summarize=20)
+            #    print("==========================")
 
+            # input_ids = tf.concat(
+            #    input_array,  # repeat num_observations times
+            #    axis=1,
+            # )
+            # sys.exit(0)
             # logging.info("input_ids shape: %s", input_ids.shape)
             # tf.print(input_ids[0])
             return input_ids, discrete_array
@@ -215,7 +229,11 @@ class DataLoader:
         return tf.reshape(columns, (row_len, -1))
 
     def load(
-        self, mask_image=False, mask_continuous=False, mask_discrete=False, sliding_batch_size=4
+        self,
+        mask_image=False,
+        mask_continuous=False,
+        mask_discrete=False,
+        sliding_batch_size=4,
     ):
         """Load data from JSON files"""
 

simple-transformer.py
@@ -8,21 +8,39 @@ from gato import Gato
 import tensorflow as tf
 from config import GatoConfig
 from loader import DataLoader
+from tensorflow.keras import losses
+
+cce = losses.CategoricalCrossentropy(from_logits=True, axis=0)
+sce = losses.SparseCategoricalCrossentropy(from_logits=True)
 
 
 def loss_function(y_true, y_pred):
-    print("y_true shape: ", y_true.shape)
-    tf.print("y_true: ", y_true)
+    global cce
+    global sce
+
+    y_pred = tf.squeeze(y_pred, axis=0)
+    y_true = tf.squeeze(y_true, axis=0)
+
+    y_pred = tf.squeeze(y_pred, axis=0)
+    y_true = tf.squeeze(y_true, axis=0)
+
+    # print("y_true shape: ", y_true.shape)
+    # tf.print("y_true: ", y_true)
 
     print("y_pred shape: ", y_pred.shape)
     tf.print("y_pred: ", y_pred)
 
-    print("[dice_loss] y_pred=", y_pred, "y_true=", y_true)
-    y_true = tf.cast(y_true, tf.float32)
-    numerator = 2 * tf.reduce_sum(y_true * y_pred)
-    denominator = tf.reduce_sum(y_true + y_pred)
+    y_label = tf.argmax(y_true, axis=0)
+    print("y_label shape: ", y_label.shape)
+    tf.print("y_label: ", y_label)
 
-    return 1 - numerator / denominator
+    losses = sce(y_label, y_pred)
+
+    # losses = cce(y_true, y_pred)
+    print("losses shape: ", losses.shape)
+    tf.print("losses: ", losses)
+
+    return losses
 
 
 if __name__ == "__main__":
@@ -33,15 +51,25 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--batch_size", type=int, default=6)
     parser.add_argument("--sliding_batch_size", type=int, default=4)
+    parser.add_argument(
+        "--exp_config",
+        type=str,
+        default="experiments/Cuboid100Episodes/MasterJsonForEpisodes2023-12-08_22-24-37-645.json",
+    )
     args = parser.parse_args()
 
     logging.info("Loading data")  # Hardcoded for now
-    file = "experiments/Cuboid100Episodes/MasterJsonForEpisodes2023-12-08_22-24-37-645.json"
-    data = DataLoader(file)
+    # file = "experiments/Cuboid100Episodes/MasterJsonForEpisodes2023-12-08_22-24-37-645.json"
+    data = DataLoader(args.exp_config)
 
-    (input_tokens, sequence_encoding, row_pos, col_pos, obs_encoding, all_discrete) = data.load(
-        sliding_batch_size=args.sliding_batch_size, mask_discrete=True
-    )
+    (
+        input_tokens,
+        sequence_encoding,
+        row_pos,
+        col_pos,
+        obs_encoding,
+        all_discrete,
+    ) = data.load(sliding_batch_size=args.sliding_batch_size, mask_discrete=False)
 
     config = GatoConfig.small()
     gato_model = Gato(config, trainable=True, name="Gato")
@@ -64,22 +92,27 @@ if __name__ == "__main__":
     # y_train = np.random.randint(3, size=(input_tokens.shape[0], 1, 3))
 
     y_train = all_discrete
-    logging.info("y_train shape: ", y_train.shape)
+    logging.info("y_train shape: %s", y_train.shape)
     # tf.print(y_train.numpy())
 
     logging.info("Compiling model ================")
-    # gato_model.compile(
-    #     optimizer=tf.keras.optimizers.Adam(), loss=loss_function, metrics=["accuracy"]
-    # )
     gato_model.compile(
-        optimizer=tf.keras.optimizers.AdamW(), loss="categorical_crossentropy", metrics=["accuracy"]
+        optimizer=tf.keras.optimizers.AdamW(), loss=loss_function, metrics=["accuracy"]
     )
+    # gato_model.compile(
+    #    optimizer=tf.keras.optimizers.AdamW(), loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    # )
 
     # gato_model(x_train)
 
     logging.info("Training the model ================")
     gato_model.fit(
-        x_train, y_train, epochs=args.epochs, batch_size=args.batch_size, verbose=2, shuffle=True
+        x_train,
+        y_train,
+        epochs=args.epochs,
+        batch_size=args.batch_size,
+        verbose=2,
+        shuffle=True,
     )
     print("model summary: ", gato_model.summary(expand_nested=False))
     # print("Running model.evaluate =================")